65e6a3fd94198c43c87f63b89e7fe04b47134870
johnny.chen <johnny.chen@thekono.com>
2023-03-04 15:09:27 +0800

    test on every push

--- cicd/__tests__/index.test.ts
+++ cicd/__tests__/index.test.ts
@@ -1,7 +1,7 @@
 import  testFunc  from '@/lib/lib_func'
 
 describe('styFunc 2', () => {
-  it('plus 2', () => {
-    expect(testFunc(1)).toBe(3)
+  it('plus 1', () => {
+    expect(testFunc(1)).toBe(2)
   })
 })
